e6a1e61be5c79ae6a6ee973d06a51fdcf3042a28
Deepak Kumar <deepaklg02@gmail.com>
2024-06-05 18:08:24 +0530

    errors

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,10 +1,11 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../App.css";
 import Button from "../components/Button";
 import { useUser } from "../UserContext";
 const Login: React.FC = () => {
   const navigate = useNavigate();
+  const [message, setMessage] = useState<boolean>(false);
   const { setUserData } = useUser();
   const handleFormSubmit = async (e: FormEvent) => {
     e.preventDefault();
@@ -16,7 +17,7 @@ const Login: React.FC = () => {
       return;
     }
     try {
-      const response = await fetch("https://levitation-back.vercel.app/login", {
+      const response = await fetch("https://levitation-back.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -28,6 +29,8 @@ const Login: React.FC = () => {
       console.log(data);
       if (!response.ok) {
         throw new Error(`Error: ${response.status}`);
+      } else {
+        setMessage(true);
       }
       setUserData(data.admin);
       if (data.message === "Admin not found") {
@@ -51,7 +54,7 @@ const Login: React.FC = () => {
         </div>
       </div>
       <form
-        className="absolute flex flex-col gap-2 p-4 md:p-10 w-full md:w-4/5 lg:w-1/3 top-1/4 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-90 rounded-md transition-opacity duration-1000 text-white z-10"
+        className="absolute flex flex-col gap-2 p-4 md:p-10 w-full h-1/2  md:w-4/5 lg:w-1/3 top-1/4 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-90 rounded-md transition-opacity duration-1000 text-white z-10"
         onSubmit={handleFormSubmit}
       >
         <label htmlFor="login">
@@ -82,6 +85,11 @@ const Login: React.FC = () => {
           </Button>
         </Link>
       </form>
+      {message && (
+        <div className="absolute flex items-center justify-center w-full h-full bg-black bg-opacity-90">
+          <h1 className="text-2xl text-white">Logging in...</h1>
+        </div>
+      )}
     </div>
   );
 };
