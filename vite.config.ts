fff79baff77debe4575310432d525f73eddefaa0
Deepak Kumar <deepaklg02@gmail.com>
2024-06-06 15:04:12 +0530

    added hamburger

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Add this line
+  },
   base: "/",
   build: {
     outDir: "dist",
